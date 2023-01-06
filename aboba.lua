529966390e66134c577bd0fed57a341d9474b3d0
TpaBaBpyKe <116580153+TpaBaBpyKe@users.noreply.github.com>
2023-01-06 23:40:11 +0300

    Update aboba.lua

--- aboba.lua
+++ aboba.lua
@@ -766,7 +766,14 @@ Players.LocalPlayer.leaderstats.Raised.Changed:Connect(function()
     if getgenv().settings.webhookToggle and getgenv().settings.webhookBox then
         local LogService = Game:GetService("LogService")
         local logs = LogService:GetLogHistory()
-        webhook(tostring("╔🛎Пришёл донат!🛎\n║💸 Поднято ⏣: " .. tostring(Players.LocalPlayer.leaderstats.Raised.Value - RaisedC) .. '\n║💸Всего ⏣: ' .. tostring(Players.LocalPlayer.leaderstats.Raised.Value) ..'\n╚:bust_in_silhouette:Аккаунт: ' .. Players.LocalPlayer.DisplayName .. ' (' .. Players.LocalPlayer.Name .. ')'))
+
+        local a = 3
+        local a11 = ((a*40/100)-a)
+        local a12 = (a - a11)
+        local a13 = (a12 - a)
+        local a14 = (math.floor (a13+0.5))
+        
+        webhook(tostring("╔🛎Пришёл донат!🛎\n║💸 Поднято ⏣: " .. tostring(Players.LocalPlayer.leaderstats.Raised.Value - RaisedC) .. '\n║💸Всего ⏣: ' .. tostring(Players.LocalPlayer.leaderstats.Raised.Value) ..'\n║:bust_in_silhouette:Аккаунт: ' .. Players.LocalPlayer.DisplayName .. ' (' .. Players.LocalPlayer.Name .. ')'.. '\n╚💲После комиссии: ' .. a14))
     end
     if getgenv().settings.donationJump then
         task.spawn(function()
@@ -788,7 +795,7 @@ end
 end)
 update()
 
-game:GetService("CoreGui").imgui.Windows.Window.Title.Text = "PLS DONATE - TpaBa"
+game:GetService("CoreGui").imgui.Windows.Window.Title.Text = "PLS DONATE - TpaBa v0.91"
 
 while task.wait(getgenv().settings.serverHopDelay * 60) do
     if not hopTimer then
